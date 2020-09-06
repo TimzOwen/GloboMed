d08e5f749bd1511afdf26ae4daf8c45297fa4768
TimzOwen <ktimothy@kabarak.ac.ke>
2020-09-06 04:51:05 +0300

    query update

--- app/src/main/java/com/owen/globomed/DatabaseHelper.kt
+++ app/src/main/java/com/owen/globomed/DatabaseHelper.kt
@@ -4,17 +4,17 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class DatabaseHelper(
-    context: Context?,
-    name: String?,
-    factory: SQLiteDatabase.CursorFactory?,
-    version: Int
-) : SQLiteOpenHelper(context, name, factory, version) {
+class DatabaseHelper(context: Context?): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     override fun onCreate(db: SQLiteDatabase?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+    //initialize db name and version
+    companion object{
+        const val DATABASE_NAME = "globomed.db"
+        const val DATABASE_VERSION = 1
     }
 }
--- app/src/main/java/com/owen/globomed/GloboMedContract.kt
+++ app/src/main/java/com/owen/globomed/GloboMedContract.kt
@@ -3,6 +3,15 @@ package com.owen.globomed
 import android.provider.BaseColumns
 
 object GloboMedContract {
+
+    //create the Query here
+    const val SQL_CREATE_ENTRIES: String = "" +
+            "CREATE TABLE ${EmployeeEntry.TABLE_NAME}(" +
+            "${EmployeeEntry.COLUMN_ID} INTEGER PRIMARY KEY AUTO INCREMENT," +
+            "${EmployeeEntry.COLUMN_NAME} TEXT NOT NULL," +
+            "${EmployeeEntry.COLUMN_DOB} DATE, NOT NULL," +
+            "${EmployeeEntry.COLUMN_DESIGNATION} TEXT NOT NULL)"
+
     object EmployeeEntry:BaseColumns{
         const val TABLE_NAME = "employee"
         const val COLUMN_ID = "id"
