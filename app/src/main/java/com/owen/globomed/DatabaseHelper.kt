5388c60c84293bbb922b59cdc3752d9376329131
TimzOwen <ktimothy@kabarak.ac.ke>
2020-09-06 04:40:01 +0300

    SQL helper class update

---
+++ app/src/main/java/com/owen/globomed/DatabaseHelper.kt
@@ -0,0 +1,20 @@
+package com.owen.globomed
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(
+    context: Context?,
+    name: String?,
+    factory: SQLiteDatabase.CursorFactory?,
+    version: Int
+) : SQLiteOpenHelper(context, name, factory, version) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
