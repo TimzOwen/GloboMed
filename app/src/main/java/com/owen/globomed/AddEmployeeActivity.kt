565465a982c60b1a1a4e2e9c4c89a39915bc86fd
TimzOwen <ktimothy@kabarak.ac.ke>
2020-09-08 21:54:15 +0300

    new data entry

--- app/src/main/java/com/owen/globomed/AddEmployeeActivity.kt
+++ app/src/main/java/com/owen/globomed/AddEmployeeActivity.kt
@@ -1,11 +1,42 @@
 package com.owen.globomed
 
+import android.app.Activity
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_employee.*
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddEmployeeActivity : AppCompatActivity() {
+class AddEmployeeActivity : Activity() {
+
+    private val myCalender : Calendar = Calendar.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_employee)
+
+        val date = DatePickerDialog.OnDateSetListener{view, year, month, dayOfMonth ->
+            myCalender.set(Calendar.YEAR, year)
+            myCalender.set(Calendar.MONTH,month)
+            myCalender.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+
+            etDoBirth.setText(getFormatedDate(myCalender.timeInMillis))
+        }
+        etDoBirth.setOnClickListener {
+            setUpCalenderDate(date)
+        }
+    }
+
+    private fun setUpCalenderDate(date: DatePickerDialog.OnDateSetListener) {
+        DatePickerDialog(this,date,myCalender.get(Calendar.YEAR), myCalender.get(Calendar.MONTH),
+            myCalender.get(Calendar.DAY_OF_MONTH)).show()
+    }
+    private fun getFormatedDate(dobInMilis: Long): String {
+        return dobInMilis?.let {
+            val sdf = SimpleDateFormat("d MMM yyyy", Locale.getDefault())
+            sdf.format(dobInMilis)
+        }?: "Not Found"
     }
 }
--- app/src/main/java/com/owen/globomed/MainActivity.kt
+++ app/src/main/java/com/owen/globomed/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.owen.globomed
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       fab.setOnClickListener {
+           val addEmployee = Intent(this,AddEmployeeActivity::class.java)
+           startActivityForResult(addEmployee, 1)
+       }
     }
 }
